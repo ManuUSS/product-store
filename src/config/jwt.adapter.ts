22a0cca1b7f749853368bbae077345f4d5febddb
Manuel Ulate <fr3zs09@gmail.com>
2024-11-10 11:52:59 -0600

    feat:JWT Seed implemented

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,13 +1,14 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
 
-
+const JWT_SEED = envs.JWT_SECRET;
 export class JWTAdapter {
 
 
   static async generateToken( payload:Record<string, any>, duration:string = '2h' ) {
     
     return new Promise(( resolve ) => {
-      jwt.sign( payload, "SEED", { expiresIn: duration }, (err, token) => {
+      jwt.sign( payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
         
         if ( err ) resolve( null );
         
