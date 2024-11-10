30e6e64da25aaf1895d3d1e34bb9b4272ee199c0
Manuel Ulate <fr3zs09@gmail.com>
2024-11-10 11:45:15 -0600

    feat(config):jwt adapter

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -4,12 +4,17 @@ import jwt from 'jsonwebtoken';
 export class JWTAdapter {
 
 
-  static generateToken( payload:Record<string, any>, duration:string = '2h' ):string {
+  static async generateToken( payload:Record<string, any>, duration:string = '2h' ) {
     
-
-
-
-    return 'any';
+    return new Promise(( resolve ) => {
+      jwt.sign( payload, "SEED", { expiresIn: duration }, (err, token) => {
+        
+        if ( err ) resolve( null );
+        
+        resolve( token );
+      
+      });
+    });
 
   };
 
