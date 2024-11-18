71719b53ee8a9564f33c2a66fbd450e2e263bc3d
Manuel Ulate <fr3zs09@gmail.com>
2024-11-18 09:11:11 -0600

    dependencies:uuid and express-fileupload

---
+++ src/presentation/services/file-upload.service.ts
@@ -0,0 +1,31 @@
+
+
+export class FileUploadService {
+
+
+  constructor(){};
+
+
+  private checkFolder( folderPath:string ){
+
+  };
+
+  public uploadSingle( 
+    file:File, 
+    folder:string = 'upload', 
+    validExtensions:string[] = ['png', 'jpg', 'jpeg', 'gif']
+  ){
+
+  };
+
+  public uploadMultiple(
+    file:File[], 
+    folder:string = 'upload', 
+    validExtensions:string[] = ['png', 'jpg', 'jpeg', 'gif']
+  ){
+
+  };
+
+};
+
+
