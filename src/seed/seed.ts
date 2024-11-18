05ee21bd80bb239f4976702a7dcdb4138dab8769
Manuel Ulate <fr3zs09@gmail.com>
2024-11-18 08:54:07 -0600

    feat(seed):database populate different collections

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -1,5 +1,8 @@
-import { envs } from "../config/envs";
-import { MongoDatabase } from "../data";
+import { envs } from '../config/envs';
+import { MongoDatabase, UserModel } from '../data';
+import { CategoryModel } from '../data/mongo/models/category.model';
+import { ProductModel } from '../data/mongo/models/product.model';
+import { seedData } from './data';
 
 
 (async () => {
@@ -14,7 +17,34 @@ import { MongoDatabase } from "../data";
 
 })();
 
+const randomBetween0andMax = ( max:number ) => Math.floor( Math.random() * max ); 
+
 async function main() {
   
+  // Delete all data from collections
+  await Promise.all([
+    UserModel.deleteMany(),
+    CategoryModel.deleteMany(),
+    ProductModel.deleteMany(),
+  ]);
+
+  // Inserts users data
+  const users = await UserModel.insertMany( seedData.users );
+
+  const categories = await CategoryModel.insertMany( 
+    seedData.categories.map(( category ) => ({ 
+      ...category, 
+      user: users[ 0 ]._id 
+    })) 
+  );
+
+  await ProductModel.insertMany( 
+    seedData.products.map(( product ) => ({ 
+      ...product, 
+      user: users[randomBetween0andMax( users.length - 1 )]._id,
+      category: categories[randomBetween0andMax( categories.length - 1 )]._id 
+    })) 
+  );
+
 }
 
